25f9ffac3d5cf820f41f046cc30b3818efb0b5b3
Ali <74619504+Utinax@users.noreply.github.com>
2021-02-14 20:11:37 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,18 @@
+Config = {}
+
+Config.Mythic_SendAlert = false -- True yaparsanız mythic_notify SendAlert ile gider, false yaparsanız DoHudText.
+
+Config.VergiBolum = 25 -- Aracın vergisini hesaplarken araç fiyatının kaça bölüneceği. Örn: 30 ise aracın fiyatı / 30 = aracın günlük vergisi.
+Config.SinirVergiBolum = 2 -- Aracın sınır vergisini hesaplarken kaça bölüneceği. Örn: 2 ise aracın fiyatı / 2 = aracın sınır vergisi.
+
+Config.VergiDairesi = {x = -250.308, y = 6159.350, z = 31.476}
+
+Config.Saat = 12
+Config.Dakika = 50
+
+Config.Blip = {
+    sprite = 162,
+    display = 4,
+    scale = 0.6,
+    colour = 3
+}
